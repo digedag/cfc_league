554d4ec478ecfaf92c9b04d5b6494df1a0d87276
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2008-06-30 17:13:26 +0000

    teamnotes with saveDocNew button

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,6 +18,9 @@ t3lib_extMgm::addUserTSConfig('
 t3lib_extMgm::addUserTSConfig('
 	options.saveDocNew.tx_cfcleague_profiles=1
 ');
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_cfcleague_team_notes=1
+');
 
 // Der TCE-Hook für das Anlegen neuer Spieler
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:' . $_EXTKEY . '/mod1/class.tx_cfcleague_mod1_tcehook.php:tx_cfcleague_mod1_tcehook';
