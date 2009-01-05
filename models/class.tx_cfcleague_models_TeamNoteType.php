267a162fe0e22e1a4fbe297360162f0a843d02d0
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2009-01-05 20:21:36 +0000

    First model classes

---
+++ models/class.tx_cfcleague_models_TeamNoteType.php
@@ -0,0 +1,101 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Rene Nitzsche (rene@system25.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+tx_div::load('tx_rnbase_model_base');
+
+/**
+ * Model for a note type.
+ */
+class tx_cfcleague_models_TeamNoteType extends tx_rnbase_model_base {
+  private static $instances = array();
+
+  function getTableName(){return 'tx_cfcleague_note_types';}
+
+  /**
+   * Liefert den Namen des Markers
+   *
+   * @return string
+   */
+  function getMarker() {
+  	return $this->record['marker'];
+  }
+  /**
+   * Liefert den Namen des Types
+   *
+   * @return string
+   */
+  function getLabel() {
+  	return $this->record['label'];
+  }
+  /**
+   * Liefert die Beschreibung des Types
+   *
+   * @return string
+   */
+  function getDescription() {
+  	return $this->record['description'];
+  }
+  /**
+   * Liefert die Instance eines Landes
+   *
+   * @param int $uid
+   * @return tx_cfcleague_models_TeamNoteType
+   */
+  static public function getInstance($uid) {
+  	self::_init();
+  	return self::$instances[$uid];
+  }
+  /**
+   * Returns an array with all types
+   *
+   * @return array
+   */
+  static public function getAll() {
+  	self::_init();
+  	return array_values(self::$instances);
+  }
+  
+	/**
+	 * Lädt alle Instanzen aus der DB und legt sie in das Array self::$instances.
+	 * Key ist die UID des Records.
+	 */
+	private static function _init() {
+		if(count(self::$instances))
+			return;
+
+		$options['wrapperclass'] = 'tx_cfcleague_models_TeamNoteType';
+		$result = tx_rnbase_util_DB::doSelect('*','tx_cfcleague_note_types',$options, 0);
+  	
+		foreach($result As $type) {
+			self::$instances[$type->uid] = $type;
+		}
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/models/class.tx_cfcleague_models_TeamNoteType.php']) {
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/models/class.tx_cfcleague_models_TeamNoteType.php']);
+}
+
+?>
---
+++ search/class.tx_cfcleague_search_TeamNote.php
@@ -0,0 +1,75 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2006-2008 Rene Nitzsche
+ *  Contact: rene@system25.de
+ *  All rights reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ ***************************************************************/
+
+require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+
+tx_div::load('tx_rnbase_util_SearchBase');
+tx_div::load('tx_rnbase_util_Misc');
+
+
+/**
+ * Class to search team notes from database
+ * 
+ * @author Rene Nitzsche
+ */
+class tx_cfcleague_search_TeamNote extends tx_rnbase_util_SearchBase {
+
+	protected function getTableMappings() {
+		$tableMapping['TEAMNOTE'] = 'tx_cfcleague_team_notes';
+		$tableMapping['NOTETYPE'] = 'tx_cfcleague_note_types';
+		$tableMapping['TEAM'] = 'tx_cfcleague_teams';
+		// Hook to append other tables
+		tx_rnbase_util_Misc::callHook('cfc_league','search_TeamNote_getTableMapping_hook',
+			array('tableMapping' => &$tableMapping), $this);
+		return $tableMapping;
+	}
+
+	protected function getBaseTable() {
+		return 'tx_cfcleague_team_notes';
+	}
+	function getWrapperClass() {
+		return 'tx_cfcleague_models_TeamNote';
+	}
+
+	protected function getJoins($tableAliases) {
+		$join = '';
+		if(isset($tableAliases['TEAM'])) {
+			$join .= ' JOIN tx_cfcleague_teams ON tx_cfcleague_teams.uid = tx_cfcleague_team_notes.team ';
+		}
+		if(isset($tableAliases['NOTETYPE'])) {
+			$join .= ' JOIN tx_cfcleague_note_types ON tx_cfcleague_note_types.uid = tx_cfcleague_team_notes.type ';
+		}
+		
+		// Hook to append other tables
+		tx_rnbase_util_Misc::callHook('cfc_league','search_TeamNote_getJoins_hook',
+			array('join' => &$join, 'tableAliases' => $tableAliases), $this);
+		return $join;
+	}
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/search/class.tx_cfcleague_search_TeamNote.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/search/class.tx_cfcleague_search_TeamNote.php']);
+}
+
+?>
