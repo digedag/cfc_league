829d572138f61bfeef391eb2272fd119d9a61f56
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2009-11-07 11:30:42 +0000

    Einfacher ObjektCache

---
+++ util/class.tx_cfcleague_util_Cache.php
@@ -0,0 +1,55 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Rene Nitzsche (rene@system25.de)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('div') . 'class.tx_div.php');
+
+
+/**
+ * Object-Cache
+ */
+class tx_cfcleague_util_Cache {
+	private static $cache = array();
+  /**
+   * Ein Objekt in den Cache legen
+   * @param string $key
+   * @param mixed $value
+   */
+  public static function add($key,$value){
+  	self::$cache[$key] = $value;
+  }
+  /**
+   * Liefert ein Objekt aus dem Cache
+   * @param $key
+   * @return mixed
+   */
+  public static function get($key){
+  	return self::$cache[$key];
+  }
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/util/class.tx_cfcleague_util_Cache.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/util/class.tx_cfcleague_util_Cache.php']);
+}
+
+?>
