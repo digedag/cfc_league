f2ec81174e7c8afedd5323c6130def316285b647
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2010-11-08 10:50:29 +0000

    Missing include added

--- services/class.tx_cfcleague_services_Group.php
+++ services/class.tx_cfcleague_services_Group.php
@@ -25,7 +25,7 @@
 require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 require_once(PATH_t3lib.'class.t3lib_svbase.php');
 
-
+tx_rnbase::load('tx_rnbase_cache_Manager');
 /**
  * Service for accessing age groups
  * 
