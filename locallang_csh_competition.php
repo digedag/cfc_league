84247c0acd24f1b8552ba0977547e497950c0563
René Nitzsche <rene@system25.de>
2015-11-07 15:29:05 +0100

    Match keys for league with 20 teams

--- locallang_csh_competition.php
+++ locallang_csh_competition.php
@@ -2,7 +2,7 @@
 $LOCAL_LANG = Array(
   'default' => Array(
     '.description' => '
-      A competition collects a number of matches. You can define a competition as 
+      A competition collects a number of matches. You can define a competition as
       league, cup or friendly matches. Most details are used for leagues only. ',
     'match_keys.description' => 'See details for some example keysets used in Germany.',
     'match_keys.details' => '
@@ -39,25 +39,30 @@ $LOCAL_LANG = Array(
 18 Teams:
 ---------
 1-17,3-14,5-12,7-10,9-8,11-6,13-4,15-2,18-16|2-13,4-11,6-9,8-7,10-5,12-3,14-18,16-1,17-15|1-15,3-10,5-8,7-6,9-4,11-2,13-17,16-14,18-12|2-9,4-7,6-5,8-3,10-18,12-16,14-1,15-13,17-11|1-13,3-6,5-4,7-2,9-17,11-15,14-12,16-10,18-8|2-5,4-3,6-18,8-16,10-14,12-1,13-11,15-9,17-7|1-11,3-2,5-17,7-15,9-13,12-10,14-8,16-6,18-4|2-18,4-16,6-14,8-12,10-1,11-9,13-7,15-5,17-3|1-9,3-15,5-13,7-11,10-8,12-6,14-4,16-2,18-17|2-14,4-12,6-10,8-1,9-7,11-5,13-3,15-18,17-16|1-7,3-11,5-9,8-6,10-4,12-2,14-17,16-15,18-13|2-10,4-8,6-1,7-5,9-3,11-18,13-16,15-14,17-12|1-5,3-7,6-4,8-2,10-17,12-15,14-13,16-11,18-9|2-6,4-1,5-3,7-18,9-16,11-14,13-12,15-10,17-8|1-3,4-2,6-17,8-15,10-13,12-11,14-9,16-7,18-5|1-2,3-18,5-16,7-14,9-12,11-10,13-8,15-6,17-4|2-17,4-15,6-13,8-11,10-9,12-7,14-5,16-3,18-1
+
+20 Teams:
+---------
+1-19,3-16,5-14,7-12,9-10,11-8,13-6,15-4,17-2,20-18|2-15,4-13,6-11,8-9,10-7,12-5,14-3,16-20,18-1,19-17|1-17,3-12,5-10,7-8,9-6,11-4,13-2,15-19,18-16,20-14|2-11,4-9,6-7,8-5,10-3,12-20,14-18,16-1,17-15,19-13|1-15,3-8,5-6,7-4,9-2,11-19,13-17,16-14,18-12,20-10|2-7,4-5,6-3,8-20,10-18,12-16,14-1,15-13,17-11,19-9|1-13,3-4,5-2,7-19,9-17,11-15,14-12,16-10,18-8,20-6|2-3,4-20,6-18,8-16,10-14,12-1,13-11,15-9,17-7,19-5|1-11,3-19,5-17,7-15,9-13,12-10,14-8,16-6,18-4,20-2|2-18,4-16,6-14,8-12,10-1,11-9,13-7,15-5,17-3,19-20|1-9,3-15,5-13,7-11,10-8,12-6,14-4,16-2,18-19,20-17|2-14,4-12,6-10,8-1,9-7,11-5,13-3,15-20,17-18,19-16|1-7,3-11,5-9,8-6,10-4,12-2,14-19,16-17,18-15,20-13|2-10,4-8,6-1,7-5,9-3,11-20,13-18,15-16,17-14,19-12|1-5,3-7,6-4,8-2,10-19,12-17,14-15,16-13,18-11,20-9|2-6,4-1,5-3,7-20,9-18,11-16,13-14,15-12,17-10,19-8|1-3,4-2,6-19,8-17,10-15,12-13,14-11,16-9,18-7,20-5|1-2,3-20,5-18,7-16,9-14,11-12,13-10,15-8,17-6,19-4|2-19,4-17,6-15,8-13,10-11,12-9,14-7,16-5,18-3,20-1
+
 </pre>
     ',
     'addparts.description' => 'Add results of match parts for end result.',
     'addparts.details' => '
-    This is useful if you want to show results of match parts in FE. 
+    This is useful if you want to show results of match parts in FE.
     For an icehockey match you can input 0:2, 1:1, 2:1. The end result would be 3:4.
     ',
 		'teams.description' => 'Select all teams that belong to this competition.',
     'teams.details' => '
-      <p><b>Note:</b> You can only create matches for this competition if there are at least 
+      <p><b>Note:</b> You can only create matches for this competition if there are at least
       two teams selected!</p>
-      <p>If you want to create a matchplan by keystring, ensure the right order of teams starting 
+      <p>If you want to create a matchplan by keystring, ensure the right order of teams starting
       by team number 1.</p>
       </p>',
     'internal_name.description' => 'This name is shown in backend only.',
     'table_marks.description' => 'Mark positions in league tables.',
     'table_marks.details' => '
 This is a simple way to include specific CSS-Markers in league tables.
-      
+
 <b>Format:</b>
 position[,position];marker,description[|position;marker,description]
 
@@ -66,21 +71,21 @@ position[,position];marker,description[|position;marker,description]
 
 The example marks position <b>1</b> with marker <b style="color:green">green</b> and positions <b>14 to 16</b> with marker <b style="color:red">red</b>. The description is optional.
 In your FE HTML-Template is a marker ###ROW_MARK###. Each time a defined position is rendered, this marker will be filled with the string you defined. The description is accessible by marker ###ROW_MARKLABEL###.
-The default template uses this marker to create a predefined CSS-Class. Have a look at the template <b>leaguetable.html</b> in <b>cfc_league_fe</b> to understand the principle. 
+The default template uses this marker to create a predefined CSS-Class. Have a look at the template <b>leaguetable.html</b> in <b>cfc_league_fe</b> to understand the principle.
     ',
   ),
 
 
   'de' => Array(
     '.description' => '
-      Ein Wettbewerb ist eine Sammlung von zusammengehörenden Spielen.  
-      Es gibt drei Arten: Liga, Pokal und Freunschaftspiele. Die meisten 
+      Ein Wettbewerb ist eine Sammlung von zusammengehï¿½renden Spielen.
+      Es gibt drei Arten: Liga, Pokal und Freunschaftspiele. Die meisten
       Angaben in diesem Wettbewerb sind nur bei Ligen notwendig.',
-    'match_keys.description' => 'In der Detailhilfe sind einige Spielplanschlüssel 
-    des DFB aufgeführt.
+    'match_keys.description' => 'In der Detailhilfe sind einige Spielplanschlï¿½ssel
+    des DFB aufgefï¿½hrt.
     ',
     'match_keys.details' => '
-<h2>Einige fertige Spielplanschlüssel des DFB</h2>
+<h2>Einige fertige Spielplanschlï¿½ssel des DFB</h2>
 <pre>
 4 Teams:
 --------
@@ -113,35 +118,40 @@ The default template uses this marker to create a predefined CSS-Class. Have a l
 18 Teams:
 ---------
 1-17,3-14,5-12,7-10,9-8,11-6,13-4,15-2,18-16|2-13,4-11,6-9,8-7,10-5,12-3,14-18,16-1,17-15|1-15,3-10,5-8,7-6,9-4,11-2,13-17,16-14,18-12|2-9,4-7,6-5,8-3,10-18,12-16,14-1,15-13,17-11|1-13,3-6,5-4,7-2,9-17,11-15,14-12,16-10,18-8|2-5,4-3,6-18,8-16,10-14,12-1,13-11,15-9,17-7|1-11,3-2,5-17,7-15,9-13,12-10,14-8,16-6,18-4|2-18,4-16,6-14,8-12,10-1,11-9,13-7,15-5,17-3|1-9,3-15,5-13,7-11,10-8,12-6,14-4,16-2,18-17|2-14,4-12,6-10,8-1,9-7,11-5,13-3,15-18,17-16|1-7,3-11,5-9,8-6,10-4,12-2,14-17,16-15,18-13|2-10,4-8,6-1,7-5,9-3,11-18,13-16,15-14,17-12|1-5,3-7,6-4,8-2,10-17,12-15,14-13,16-11,18-9|2-6,4-1,5-3,7-18,9-16,11-14,13-12,15-10,17-8|1-3,4-2,6-17,8-15,10-13,12-11,14-9,16-7,18-5|1-2,3-18,5-16,7-14,9-12,11-10,13-8,15-6,17-4|2-17,4-15,6-13,8-11,10-9,12-7,14-5,16-3,18-1
+
+20 Teams:
+---------
+1-19,3-16,5-14,7-12,9-10,11-8,13-6,15-4,17-2,20-18|2-15,4-13,6-11,8-9,10-7,12-5,14-3,16-20,18-1,19-17|1-17,3-12,5-10,7-8,9-6,11-4,13-2,15-19,18-16,20-14|2-11,4-9,6-7,8-5,10-3,12-20,14-18,16-1,17-15,19-13|1-15,3-8,5-6,7-4,9-2,11-19,13-17,16-14,18-12,20-10|2-7,4-5,6-3,8-20,10-18,12-16,14-1,15-13,17-11,19-9|1-13,3-4,5-2,7-19,9-17,11-15,14-12,16-10,18-8,20-6|2-3,4-20,6-18,8-16,10-14,12-1,13-11,15-9,17-7,19-5|1-11,3-19,5-17,7-15,9-13,12-10,14-8,16-6,18-4,20-2|2-18,4-16,6-14,8-12,10-1,11-9,13-7,15-5,17-3,19-20|1-9,3-15,5-13,7-11,10-8,12-6,14-4,16-2,18-19,20-17|2-14,4-12,6-10,8-1,9-7,11-5,13-3,15-20,17-18,19-16|1-7,3-11,5-9,8-6,10-4,12-2,14-19,16-17,18-15,20-13|2-10,4-8,6-1,7-5,9-3,11-20,13-18,15-16,17-14,19-12|1-5,3-7,6-4,8-2,10-19,12-17,14-15,16-13,18-11,20-9|2-6,4-1,5-3,7-20,9-18,11-16,13-14,15-12,17-10,19-8|1-3,4-2,6-19,8-17,10-15,12-13,14-11,16-9,18-7,20-5|1-2,3-20,5-18,7-16,9-14,11-12,13-10,15-8,17-6,19-4|2-19,4-17,6-15,8-13,10-11,12-9,14-7,16-5,18-3,20-1
+
 </pre>
     ',
     'addparts.description' => 'Addiere Teilergebnisse zum Endergebnis.',
     'addparts.details' => '
-    Das ist sinnvoll, wenn man die Teilergebnisse der einzelnen Spielabschnitte im FE anzeigen 
-    will. Dies ist in einigen Sportarten üblich. Bei einem Eishockeyspiel
-    gibt man 0:2, 1:1, 2:1 ein. Das Endergebnis wäre dann 3:4.
+    Das ist sinnvoll, wenn man die Teilergebnisse der einzelnen Spielabschnitte im FE anzeigen
+    will. Dies ist in einigen Sportarten ï¿½blich. Bei einem Eishockeyspiel
+    gibt man 0:2, 1:1, 2:1 ein. Das Endergebnis wï¿½re dann 3:4.
     ',
   	'teams.description' => 'Geben Sie alle Teams zu diesem Wettbewerb an.',
     'teams.details' => '
-      <p><b>Hinweis:</b> Für einen Wettbewerb können nur Spiele angelegt werden, wenn mindestens 
+      <p><b>Hinweis:</b> Fï¿½r einen Wettbewerb kï¿½nnen nur Spiele angelegt werden, wenn mindestens
       zwei Teams zugeordnet sind!</p>
-      <p>Wenn ein Spielplan über den Schlüssel angelegt werden soll, dann ist die Reihenfolge der 
+      <p>Wenn ein Spielplan ï¿½ber den Schlï¿½ssel angelegt werden soll, dann ist die Reihenfolge der
       Teams entscheiden. Team 1 steht ganz oben, dann Team 2, 3 usw.</p>
       </p>',
     'internal_name.description' => 'Dieser Name wird nur im Backend verwendet.',
     'table_marks.description' => 'Markierung Plazierungen in der Ligatabelle.',
     'table_marks.details' => '
-Dieses Feld bietet eine einfache Möglichkeit bestimmte Tabellenplätze in der Ligatabelle hervorzuheben.
-      
+Dieses Feld bietet eine einfache Mï¿½glichkeit bestimmte Tabellenplï¿½tze in der Ligatabelle hervorzuheben.
+
 <b>Format:</b>
 position[,position];marker,beschreibung[|position;marker,beschreibung]
 
 <b>Beispiel:</b>
 1;gruen,Meister|14,15,16;rot,Abstieg
 
-Im Beispiel wird also Tabellenplatz <b>1</b> als <b style="color:green">grün</b> und die Plätze <b>14 bis 16</b> <b style="color:red">rot</b> markiert. 
-Dafür steht im HTML-Template der Marker ###ROW_MARK### bereit. Wird eine hier angegebene Tabellenplatzierung ausgegeben, dann wird der Marker mit dem entsprechenden String gefüllt. Die Beschreibung ist über den Marker ###ROW_MARKLABEL### erreichbar.
-Im mitgelieferten Template wird der Marker verwendet, um eine bestimmte CSS-Klasse anzusprechen. Diese muss dann natürlich auch vorhanden sein, damit die Formatierung gelingt.
+Im Beispiel wird also Tabellenplatz <b>1</b> als <b style="color:green">grï¿½n</b> und die Plï¿½tze <b>14 bis 16</b> <b style="color:red">rot</b> markiert.
+Dafï¿½r steht im HTML-Template der Marker ###ROW_MARK### bereit. Wird eine hier angegebene Tabellenplatzierung ausgegeben, dann wird der Marker mit dem entsprechenden String gefï¿½llt. Die Beschreibung ist ï¿½ber den Marker ###ROW_MARKLABEL### erreichbar.
+Im mitgelieferten Template wird der Marker verwendet, um eine bestimmte CSS-Klasse anzusprechen. Diese muss dann natï¿½rlich auch vorhanden sein, damit die Formatierung gelingt.
     ',
   ),
 );
