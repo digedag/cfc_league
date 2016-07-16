fd7c6ea37f90e80d7d17fa321e7a20ef1df12c82
René Nitzsche <rene@system25.de>
2016-07-16 17:49:48 +0200

    Cleanup

--- mod1/conf.php
+++ mod1/conf.php
@@ -1,5 +1,4 @@
 <?php
-global $_EXTKEY;
 
 	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
 $MCONF['name']='web_txcfcleagueM1';
@@ -9,14 +8,6 @@ $MCONF['access']='user,group';
 $MLANG['default']['tabs_images']['tab'] = '../icon_table.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:cfc_league/mod1/locallang_mod.xml';
 
-
-// define('TYPO3_MOD_PATH', '../typo3conf/ext/cfc_league/mod1/');
-// //$BACK_PATH='../../../';
-// $BACK_PATH='../../../../typo3/';
-
-// $MCONF['script']='index.php';
-
-//$MLANG['default']['tabs_images']['tab'] = '../moduleicon.gif';
 
 // Mögliche Icons im BE für die Funktion doc->icons()
 define('ICON_OK', -1);
