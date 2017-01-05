883ff2aed9ddbf2704d3626e8b4d4b0c53dfe73f
René Nitzsche <rene@system25.de>
2017-01-05 22:15:28 +0100

    digedag/cfc_league_fe#14 model for age group refactored

--- models/class.tx_cfcleague_models_Group.php
+++ models/class.tx_cfcleague_models_Group.php
@@ -29,42 +29,38 @@ tx_rnbase::load('tx_rnbase_model_base');
  */
 class tx_cfcleague_models_Group extends tx_rnbase_model_base {
 
-  function getTableName(){return 'tx_cfcleague_group';}
+	function getTableName(){return 'tx_cfcleague_group';}
 
-  /**
-   * Liefert den Namen
-   *
-   * @return string
-   */
-  function getName() {
-  	return $this->record['name'];
-  }
+	/**
+	 * Liefert den Namen
+	 *
+	 * @return string
+	 */
+	function getName() {
+		return $this->getProperty('name');
+	}
 
-  /**
-   * Returns an instance
-   * @param int $uid
-   * @return tx_cfcleague_models_Group
-   */
-  static public function getGroupInstance($uid) {
-  	return tx_cfcleague_util_ServiceRegistry::getGroupService()->getGroupByUid($uid);
-  }
+	/**
+	 * Returns an instance
+	 * @param int $uid
+	 * @return tx_cfcleague_models_Group
+	 */
+	static public function getGroupInstance($uid) {
+		return tx_cfcleague_util_ServiceRegistry::getGroupService()->getGroupByUid($uid);
+	}
 
 	/**
 	 * statische Methode, die ein Array mit Instanzen dieser Klasse liefert. Ist der übergebene
 	 * Parameter leer, dann werden alle Datensätze aus der Datenbank geliefert. Ansonsten
 	 * wird ein String mit der uids der gesuchten Saisons erwartet ('2,4,10,...').
+	 * @return tx_cfcleague_models_Group[]
 	 */
 	static function findAll($uids) {
 		// SELECT * FROM tx_cfcleague_group WHERE uid IN ($uid)
 		$options['where'] = (is_string($uids) && strlen($uids) > 0) ? 'uid IN (' . $uids .')' : '1';
 		$options['orderby'] = 'sorting asc';
-		$options['wrapperclass'] = 'tx_cfcleaguefe_models_group';
+		$options['wrapperclass'] = 'tx_cfcleague_models_Group';
 		return  tx_rnbase_util_DB::doSelect('*', 'tx_cfcleague_group', $options);
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/models/class.tx_cfcleague_models_Group.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/models/class.tx_cfcleague_models_Group.php']);
-}
-
-?>
