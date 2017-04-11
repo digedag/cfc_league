6d14dcd4cb449609ac9c0bfd9a250416d4dd0a2d
Mario Näther <mario-naether@gmx.de>
2017-04-11 17:16:11 +0200

    - add table layout to ticker messages table

--- Classes/Controller/MatchTicker.php
+++ Classes/Controller/MatchTicker.php
@@ -116,7 +116,9 @@ class Tx_Cfcleague_Controller_MatchTicker extends tx_rnbase_mod_BaseModFunc {
 		$arr = $this->createTickerArray($match, Tx_Rnbase_Utility_T3General::_GP('showAll'));
 		if($arr) {
 			$tickerContent = $formTool->createLink('&showAll=1', $this->getModule()->getPid(), $LANG->getLL('label_showAllTickers'));
-			$tickerContent .= $this->doc->table($arr);
+            $tableLayout = $this->_getTableLayoutForm();
+            $tableLayout['defRowEven']['defCol'] = $tableLayout['defRowOdd']['defCol'];//Array('<td valign="top" style="padding:5px 5px;">', '</td>');
+			$tickerContent .= $this->doc->table($arr, $tableLayout);
 		}
 		else
 			$tickerContent .= $LANG->getLL('msg_NoTicker');
