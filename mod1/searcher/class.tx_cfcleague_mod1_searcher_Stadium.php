86e97fd677cef1e909be24d8651da4e55eec10d1
René Nitzsche <rene@system25.de>
2017-06-28 13:37:16 +0200

    Code style

--- mod1/searcher/class.tx_cfcleague_mod1_searcher_Stadium.php
+++ mod1/searcher/class.tx_cfcleague_mod1_searcher_Stadium.php
@@ -21,154 +21,187 @@
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-
 tx_rnbase::load('tx_rnbase_mod_IModule');
 tx_rnbase::load('Tx_Rnbase_Utility_T3General');
-
-
 
 /**
  * Search stadiums
  */
-class tx_cfcleague_mod1_searcher_Stadium {
-	private $mod;
-	private $data;
-	private $SEARCH_SETTINGS;
+class tx_cfcleague_mod1_searcher_Stadium
+{
 
-	private $currentClub;
+    private $mod;
 
-	/**
-	 * Constructor
-	 *
-	 * @param 	tx_rnbase_mod_IModule $mod
-	 * @param 	unknown_type $options
-	 * @return 	unknown_type
-	 */
-	public function __construct(tx_rnbase_mod_IModule $mod, $options = array()) {
-		$this->init($mod, $options);
-	}
+    private $data;
 
-	/**
-	 * Init object
-	 *
-	 * @param 	tx_rnbase_mod_IModule $mod
-	 * @param 	array $options
-	 */
-	private function init(tx_rnbase_mod_IModule $mod, $options) {
-		$this->options = $options;
-		$this->mod = $mod;
-		$this->formTool = $mod->getFormTool();
-		$this->resultSize = 0;
-		$this->data = Tx_Rnbase_Utility_T3General::_GP('searchdata');
+    private $SEARCH_SETTINGS;
 
+    private $currentClub;
 
-		if(!isset($options['nopersist']))
-			$this->SEARCH_SETTINGS = Tx_Rnbase_Backend_Utility::getModuleData(array ('searchterm' => ''), $this->data, $mod->getName() );
-		else
-			$this->SEARCH_SETTINGS = $this->data;
+    /**
+     * Constructor
+     *
+     * @param tx_rnbase_mod_IModule $mod
+     * @param unknown_type $options
+     * @return unknown_type
+     */
+    public function __construct(tx_rnbase_mod_IModule $mod, $options = array())
+    {
+        $this->init($mod, $options);
+    }
 
-	}
-	/**
-	 * Returns the complete search form
-	 */
-	public function getSearchForm() {
-		$out = '';
+    /**
+     * Init object
+     *
+     * @param tx_rnbase_mod_IModule $mod
+     * @param array $options
+     */
+    private function init(tx_rnbase_mod_IModule $mod, $options)
+    {
+        $this->options = $options;
+        $this->mod = $mod;
+        $this->formTool = $mod->getFormTool();
+        $this->resultSize = 0;
+        $this->data = Tx_Rnbase_Utility_T3General::_GP('searchdata');
 
-		return $out;
-	}
-	/**
-	 * @return tx_rnbase_mod_IModule
-	 */
-	private function getModule() {
-		return $this->mod;
-	}
+        if (! isset($options['nopersist']))
+            $this->SEARCH_SETTINGS = Tx_Rnbase_Backend_Utility::getModuleData(array(
+                'searchterm' => ''
+            ), $this->data, $mod->getName());
+        else
+            $this->SEARCH_SETTINGS = $this->data;
+    }
 
-	/**
-	 *
-	 */
-	public function getResultList() {
-		$pager = tx_rnbase::makeInstance('tx_rnbase_util_BEPager', 'stadiumPager', $this->getModule()->getName(), 0);
-		// Get stadium service
-		$srv = tx_cfcleague_util_ServiceRegistry::getStadiumService();
+    /**
+     * Returns the complete search form
+     */
+    public function getSearchForm()
+    {
+        $out = '';
 
-		// Set options
-		$options = array('count'=>1, 'distinct'=>1);
+        return $out;
+    }
 
-		$fields = array();
-		// Filter companies according to filter selections
-		if ($this->currentClub)
-			$fields['CLUB.UID'] = array(OP_EQ_INT => $this->currentClub);
+    /**
+     *
+     * @return tx_rnbase_mod_IModule
+     */
+    private function getModule()
+    {
+        return $this->mod;
+    }
 
-		if (!$this->currentShowHidden) $options['enablefieldsfe'] = 1;
-		else $options['enablefieldsbe'] = 1;
+    /**
+     */
+    public function getResultList()
+    {
+        $pager = tx_rnbase::makeInstance('tx_rnbase_util_BEPager', 'stadiumPager', $this->getModule()->getName(), 0);
+        // Get stadium service
+        $srv = tx_cfcleague_util_ServiceRegistry::getStadiumService();
 
-		// Set more options
-		$options['orderby']['STADIUM.NAME'] = 'ASC';
+        // Set options
+        $options = array(
+            'count' => 1,
+            'distinct' => 1
+        );
 
-		// Get counted data
-		$cnt = $srv->search($fields, $options);
-		unset($options['count']);
-		$pager->setListSize($cnt);
-		$pager->setOptions($options);
+        $fields = array();
+        // Filter companies according to filter selections
+        if ($this->currentClub) {
+            $fields['CLUB.UID'] = array(
+                OP_EQ_INT => $this->currentClub
+            );
+        }
 
-		// Get data
-		$items = $srv->search($fields, $options);
-		$ret = array();
-		$content = '';
-		$this->showStadiums($content, $items);
-		$ret['table'] = $content;
-		$ret['totalsize'] = $cnt;
-		$pagerData = $pager->render();
-		$ret['pager'] .= '<div class="pager">' . $pagerData['limits'] . ' - ' .$pagerData['pages'] .'</div>';
-		return $ret;
-	}
+        if (! $this->currentShowHidden) {
+            $options['enablefieldsfe'] = 1;
+        }
+        else {
+            $options['enablefieldsbe'] = 1;
+        }
 
-	/**
-	 * Start creation of result list
-	 * @param string $content
-	 * @param array $items
-	 */
-	private function showStadiums(&$content, $items) {
-		$decor = tx_rnbase::makeInstance('tx_cfcleague_mod1_decorator_Stadium', $this->getModule());
-		$columns = array(
-			'uid' => array('title' => 'label_uid', 'decorator' => $decor),
-			'name' => array('title' => 'label_name'),
-			'capacity' => array('title' => 'label_capacity'),
-			'address' => array('title' => 'label_address', 'decorator' => $decor),
-			'longlat' => array('title' => 'label_longlat', 'decorator' => $decor),
-		);
+        // Set more options
+        $options['orderby']['STADIUM.NAME'] = 'ASC';
 
-		if($items) {
-			tx_rnbase::load('Tx_Rnbase_Backend_Utility_Tables');
-			$arr = Tx_Rnbase_Backend_Utility_Tables::prepareTable($items, $columns, $this->getModule()->getFormTool(), $this->options);
-			$out = $this->getModule()->getDoc()->table($arr[0]);
-		}
-		else {
-	  	$out = '<p><strong>###LABEL_MSG_NOSTADIUMSFOUND###</strong></p><br/>';
-		}
-		$content .= $out;
-	}
+        // Get counted data
+        $cnt = $srv->search($fields, $options);
+        unset($options['count']);
+        $pager->setListSize($cnt);
+        $pager->setOptions($options);
 
-	/**
-	 * Method to get the number of data records
-	 * Works only if the result list has been retrieved
-	 *
-	 * @return 	int
-	 */
-	public function getSize() {
-		return $this->resultSize;
-	}
+        // Get data
+        $items = $srv->search($fields, $options);
+        $ret = array();
+        $content = '';
+        $this->showStadiums($content, $items);
+        $ret['table'] = $content;
+        $ret['totalsize'] = $cnt;
+        $pagerData = $pager->render();
+        $ret['pager'] .= '<div class="pager">' . $pagerData['limits'] . ' - ' . $pagerData['pages'] . '</div>';
+        return $ret;
+    }
 
-	/**
-	 * Set club filter
-	 * @param int $clubUid
-	 */
-	public function setClub($clubUid) {
-		$this->currentClub = $clubUid;
-	}
+    /**
+     * Start creation of result list
+     *
+     * @param string $content
+     * @param array $items
+     */
+    private function showStadiums(&$content, $items)
+    {
+        $decor = tx_rnbase::makeInstance('tx_cfcleague_mod1_decorator_Stadium', $this->getModule());
+        $columns = array(
+            'uid' => array(
+                'title' => 'label_uid',
+                'decorator' => $decor
+            ),
+            'name' => array(
+                'title' => 'label_name'
+            ),
+            'capacity' => array(
+                'title' => 'label_capacity'
+            ),
+            'address' => array(
+                'title' => 'label_address',
+                'decorator' => $decor
+            ),
+            'longlat' => array(
+                'title' => 'label_longlat',
+                'decorator' => $decor
+            )
+        );
+
+        if ($items) {
+            tx_rnbase::load('Tx_Rnbase_Backend_Utility_Tables');
+            $arr = Tx_Rnbase_Backend_Utility_Tables::prepareTable($items, $columns, $this->getModule()->getFormTool(), $this->options);
+            $out = $this->getModule()
+                ->getDoc()
+                ->table($arr[0]);
+        } else {
+            $out = '<p><strong>###LABEL_MSG_NOSTADIUMSFOUND###</strong></p><br/>';
+        }
+        $content .= $out;
+    }
+
+    /**
+     * Method to get the number of data records
+     * Works only if the result list has been retrieved
+     *
+     * @return int
+     */
+    public function getSize()
+    {
+        return $this->resultSize;
+    }
+
+    /**
+     * Set club filter
+     *
+     * @param int $clubUid
+     */
+    public function setClub($clubUid)
+    {
+        $this->currentClub = $clubUid;
+    }
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/mod1/searcher/class.tx_cfcleague_mod1_searcher_Stadium.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cfc_league/mod1/searcher/class.tx_cfcleague_mod1_searcher_Stadium.php']);
-}
-?>
