8bd19e13d4097b24e0cc98d96e80e5496b7f8105
René Nitzsche <nitzsche@dmk-ebusiness.de>
2017-12-03 14:00:19 +0100

    createLink fixed

--- Classes/Controller/MatchTicker.php
+++ Classes/Controller/MatchTicker.php
@@ -110,7 +110,7 @@ class Tx_Cfcleague_Controller_MatchTicker extends tx_rnbase_mod_BaseModFunc {
 		$arr = $this->createTickerArray($match, Tx_Rnbase_Utility_T3General::_GP('showAll'));
 		if($arr) {
 			$tickerContent = $formTool->createLink(
-			    ['showAll'=>1],
+			    'showAll=1',
 			    $this->getModule()->getPid(),
 			    $LANG->getLL('label_showAllTickers'),
 			    ['params' => ['showAll' => 1]]);
