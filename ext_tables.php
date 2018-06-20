d50f9ddb30ef840cc3393e65318a0dc096b3c0d4
René Nitzsche <rene@system25.de>
2018-06-20 13:26:32 +0200

    Code style

--- ext_tables.php
+++ ext_tables.php
@@ -1,28 +1,26 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-
+if (! defined('TYPO3_MODE'))
+    die('Access denied.');
 
 if (TYPO3_MODE == 'BE') {
     tx_rnbase::load('tx_cfcleague_tca_Lookup');
 }
 
-
-if(!tx_rnbase_util_TYPO3::isTYPO62OrHigher()) {
-	// TCA registration for 4.5
-	$TCA['tx_cfcleague_group'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_group.php';
-	$TCA['tx_cfcleague_saison'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_saison.php';
-	$TCA['tx_cfcleague_competition'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_competition.php';
-	$TCA['tx_cfcleague_competition_penalty'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_competition_penalty.php';
-	$TCA['tx_cfcleague_profiles'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_profiles.php';
-	$TCA['tx_cfcleague_note_types'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_note_types.php';
-	$TCA['tx_cfcleague_club'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_club.php';
-	$TCA['tx_cfcleague_teams'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_teams.php';
-	$TCA['tx_cfcleague_team_notes'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_team_notes.php';
-	$TCA['tx_cfcleague_games'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_games.php';
-	$TCA['tx_cfcleague_match_notes'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_match_notes.php';
-	$TCA['tx_cfcleague_stadiums'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/tx_cfcleague_stadiums.php';
+if (! tx_rnbase_util_TYPO3::isTYPO62OrHigher()) {
+    // TCA registration for 4.5
+    $TCA['tx_cfcleague_group'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_group.php';
+    $TCA['tx_cfcleague_saison'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_saison.php';
+    $TCA['tx_cfcleague_competition'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_competition.php';
+    $TCA['tx_cfcleague_competition_penalty'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_competition_penalty.php';
+    $TCA['tx_cfcleague_profiles'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_profiles.php';
+    $TCA['tx_cfcleague_note_types'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_note_types.php';
+    $TCA['tx_cfcleague_club'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_club.php';
+    $TCA['tx_cfcleague_teams'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_teams.php';
+    $TCA['tx_cfcleague_team_notes'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_team_notes.php';
+    $TCA['tx_cfcleague_games'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_games.php';
+    $TCA['tx_cfcleague_match_notes'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_match_notes.php';
+    $TCA['tx_cfcleague_stadiums'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Configuration/TCA/tx_cfcleague_stadiums.php';
 }
-
 
 tx_rnbase_util_Extensions::addLLrefForTCAdescr('tx_cfcleague_competition', 'EXT:cfc_league/Resources/Private/Language/locallang_csh_competition.php');
 tx_rnbase_util_Extensions::addLLrefForTCAdescr('tx_cfcleague_competition_penalty', 'EXT:cfc_league/Resources/Private/Language/locallang_csh_competition_penalty.xml');
@@ -30,70 +28,40 @@ tx_rnbase_util_Extensions::addLLrefForTCAdescr('tx_cfcleague_profiles', 'EXT:cfc
 tx_rnbase_util_Extensions::addLLrefForTCAdescr('tx_cfcleague_club', 'EXT:cfc_league/Resources/Private/Language/locallang_csh_club.php');
 tx_rnbase_util_Extensions::addLLrefForTCAdescr('tx_cfcleague_games', 'EXT:cfc_league/Resources/Private/Language/locallang_csh_games.php');
 
-
-if(tx_rnbase_util_Extensions::isLoaded('rgmediaimages')) {
-	tx_rnbase_util_Extensions::addStaticFile($_EXTKEY, 'Configurations/TypoScript/video', 'Ext: cfcleague video support');
+if (tx_rnbase_util_Extensions::isLoaded('rgmediaimages')) {
+    tx_rnbase_util_Extensions::addStaticFile($_EXTKEY, 'Configurations/TypoScript/video', 'Ext: cfcleague video support');
 }
 
-if (TYPO3_MODE=='BE')	{
+if (TYPO3_MODE == 'BE') {
 
     $modName = 'web_txcfcleagueM1';
     if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
         $modName = 'web_CfcLeagueM1';
         tx_rnbase::load('tx_cfcleague_mod1_Module');
-        tx_rnbase_util_Extensions::registerModule(
-            'cfc_league',
-            'web',
-            'M1',
-            'bottom',
-            array(),
-            array(
-                'access' => 'user,group',
-                'routeTarget' => 'tx_cfcleague_mod1_Module',
-                'icon' => 'EXT:cfc_league/Resources/Public/Icons/module-t3sports.svg',
-                'labels' => 'LLL:EXT:cfc_league/mod1/locallang_mod.xml',
-            )
-        );
+        tx_rnbase_util_Extensions::registerModule('cfc_league', 'web', 'M1', 'bottom', array(), array(
+            'access' => 'user,group',
+            'routeTarget' => 'tx_cfcleague_mod1_Module',
+            'icon' => 'EXT:cfc_league/Resources/Public/Icons/module-t3sports.svg',
+            'labels' => 'LLL:EXT:cfc_league/mod1/locallang_mod.xml'
+        ));
     } else {
-        tx_rnbase_util_Extensions::addModule(
-            'web',
-            'txcfcleagueM1',
-            '',
-            tx_rnbase_util_Extensions::extPath($_EXTKEY).'mod1/',
-            array(
-            	'labels' => array(
-              		'tabs_images' => array(
-              				'tab' => 'EXT:cfc_league/Resources/Public/Icons/module-t3sports.svg',
-              		),
-            	),
+        tx_rnbase_util_Extensions::addModule('web', 'txcfcleagueM1', '', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'mod1/', array(
+            'labels' => array(
+                'tabs_images' => array(
+                    'tab' => 'EXT:cfc_league/Resources/Public/Icons/module-t3sports.svg'
+                )
             )
-        );
+        ));
     }
-  tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Competition',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Competition.php',
-		'LLL:EXT:cfc_league/mod1/locallang.xml:mod_competition'
-	);
-  tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_MatchTicker',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/MatchTicker.php',
-		'LLL:EXT:cfc_league/mod1/locallang.xml:match_ticker'
-	);
-  tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Team',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Team.php',
-		'LLL:EXT:cfc_league/mod1/locallang.xml:mod_team'
-	);
-  tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Club',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Club.php',
-		'LLL:EXT:cfc_league/mod1/locallang.xml:mod_club'
-	);
-	tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Profile',
-		tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Profile.php',
-		'LLL:EXT:cfc_league/mod1/locallang.xml:search_profiles'
-	);
+    tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Competition', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/Competition.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:mod_competition');
+    tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_MatchTicker', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/MatchTicker.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:match_ticker');
+    tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Team', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/Team.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:mod_team');
+    tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Club', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/Club.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:mod_club');
+    tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Profile', tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/Profile.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:search_profiles');
 
-
-	tx_rnbase::load('tx_rnbase_util_TYPO3');
-	// add folder icon
-	if(tx_rnbase_util_TYPO3::isTYPO76OrHigher()) {
+    tx_rnbase::load('tx_rnbase_util_TYPO3');
+    // add folder icon
+    if (tx_rnbase_util_TYPO3::isTYPO76OrHigher()) {
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         $icons = [
             'ext-cfcleague-clubs-default' => 'icon_tx_cfcleague_clubs.gif',
@@ -107,29 +75,30 @@ if (TYPO3_MODE=='BE')	{
             'ext-cfcleague-saison-default' => 'icon_tx_cfcleague_saison.gif',
             'ext-cfcleague-statiums-default' => 'icon_table.gif',
             'ext-cfcleague-teamnotes-default' => 'icon_tx_cfcleague_teams.gif',
-            'ext-cfcleague-teams-default' => 'icon_tx_cfcleague_teams.gif',
-         ];
+            'ext-cfcleague-teams-default' => 'icon_tx_cfcleague_teams.gif'
+        ];
         foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-                ['source' => 'EXT:cfc_league/Resources/Public/Icons/' . $path]
-            );
+            $iconRegistry->registerIcon($identifier, \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, [
+                'source' => 'EXT:cfc_league/Resources/Public/Icons/' . $path
+            ]);
         }
-	}
-	elseif(tx_rnbase_util_TYPO3::isTYPO3VersionOrHigher(4004000)) {
-		t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-cfcleague', '../typo3conf/ext/cfc_league/ext_icon_cfcleague_folder.gif');
-	}
-	else {
-		$ICON_TYPES['cfcleague'] = array('icon' => tx_rnbase_util_Extensions::extRelPath($_EXTKEY).'ext_icon_cfcleague_folder.gif');
-	}
+    } elseif (tx_rnbase_util_TYPO3::isTYPO3VersionOrHigher(4004000)) {
+        t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-cfcleague', '../typo3conf/ext/cfc_league/ext_icon_cfcleague_folder.gif');
+    } else {
+        $ICON_TYPES['cfcleague'] = array(
+            'icon' => tx_rnbase_util_Extensions::extRelPath($_EXTKEY) . 'ext_icon_cfcleague_folder.gif'
+        );
+    }
 
-  $TCA['pages']['columns']['module']['config']['items'][] = array('LLL:EXT:cfc_league/locallang_db.xml:tx_cfcleague_folder', 'cfcleague');
-/*
-  $PAGES_TYPES['cfcleague'] = Array(
-      'type' => 'sys',
-      'icon' => tx_rnbase_util_Extensions::extRelPath($_EXTKEY).'ext_icon_cfcleague_folder.gif',
-      'allowedTables' => '*'
+    $TCA['pages']['columns']['module']['config']['items'][] = array(
+        'LLL:EXT:cfc_league/locallang_db.xml:tx_cfcleague_folder',
+        'cfcleague'
     );
-*/
+    /*
+     * $PAGES_TYPES['cfcleague'] = Array(
+     * 'type' => 'sys',
+     * 'icon' => tx_rnbase_util_Extensions::extRelPath($_EXTKEY).'ext_icon_cfcleague_folder.gif',
+     * 'allowedTables' => '*'
+     * );
+     */
 }
