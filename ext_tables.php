5378caf649aa64e0c0a619e03bae307ac431203a
René Nitzsche <rene@system25.de>
2020-05-22 22:15:11 +0200

    fix folder icon

--- ext_tables.php
+++ ext_tables.php
@@ -33,42 +33,34 @@ if (TYPO3_MODE == 'BE') {
     tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Club', tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Club.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:mod_club');
     tx_rnbase_util_Extensions::insertModuleFunction($modName, 'Tx_Cfcleague_Controller_Profile', tx_rnbase_util_Extensions::extPath($_EXTKEY).'Classes/Controller/Profile.php', 'LLL:EXT:cfc_league/mod1/locallang.xml:search_profiles');
 
-    tx_rnbase::load('tx_rnbase_util_TYPO3');
-    // add folder icon
-    if (tx_rnbase_util_TYPO3::isTYPO76OrHigher()) {
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        $icons = [
-            'ext-cfcleague-clubs-default' => 'icon_tx_cfcleague_clubs.gif',
-            'ext-cfcleague-competition-default' => 'icon_tx_cfcleague_competition.gif',
-            'ext-cfcleague-competition-penalty-default' => 'icon_tx_cfcleague_competition_penalty.gif',
-            'ext-cfcleague-games-default' => 'icon_table.gif',
-            'ext-cfcleague-group-default' => 'icon_tx_cfcleague_group.gif',
-            'ext-cfcleague-matchnotes-default' => 'icon_table.gif',
-            'ext-cfcleague-notetypes-default' => 'icon_table.gif',
-            'ext-cfcleague-profiles-default' => 'icon_tx_cfcleague_profiles.gif',
-            'ext-cfcleague-saison-default' => 'icon_tx_cfcleague_saison.gif',
-            'ext-cfcleague-statiums-default' => 'icon_table.gif',
-            'ext-cfcleague-teamnotes-default' => 'icon_tx_cfcleague_teams.gif',
-            'ext-cfcleague-teams-default' => 'icon_tx_cfcleague_teams.gif',
-        ];
-        foreach ($icons as $identifier => $path) {
-            $iconRegistry->registerIcon($identifier, \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, [
-                'source' => 'EXT:cfc_league/Resources/Public/Icons/'.$path,
-            ]);
-        }
-    } else {
-        t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-cfcleague', '../typo3conf/ext/cfc_league/ext_icon_cfcleague_folder.gif');
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $icons = [
+        'ext-cfcleague-clubs-default' => 'icon_tx_cfcleague_clubs.gif',
+        'ext-cfcleague-competition-default' => 'icon_tx_cfcleague_competition.gif',
+        'ext-cfcleague-competition-penalty-default' => 'icon_tx_cfcleague_competition_penalty.gif',
+        'ext-cfcleague-games-default' => 'icon_table.gif',
+        'ext-cfcleague-group-default' => 'icon_tx_cfcleague_group.gif',
+        'ext-cfcleague-matchnotes-default' => 'icon_table.gif',
+        'ext-cfcleague-notetypes-default' => 'icon_table.gif',
+        'ext-cfcleague-profiles-default' => 'icon_tx_cfcleague_profiles.gif',
+        'ext-cfcleague-saison-default' => 'icon_tx_cfcleague_saison.gif',
+        'ext-cfcleague-statiums-default' => 'icon_table.gif',
+        'ext-cfcleague-teamnotes-default' => 'icon_tx_cfcleague_teams.gif',
+        'ext-cfcleague-teams-default' => 'icon_tx_cfcleague_teams.gif',
+        'ext-cfcleague-ext-default' => 'module-t3sports.svg',
+        'ext-cfcleague-folder-default' => 't3sports-folder-contains.svg',
+    ];
+    foreach ($icons as $identifier => $path) {
+        $iconRegistry->registerIcon($identifier, \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class, [
+            'source' => 'EXT:cfc_league/Resources/Public/Icons/'.$path,
+        ]);
     }
 
-    $TCA['pages']['columns']['module']['config']['items'][] = array(
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-cfcleague'] = 'ext-cfcleague-folder-default';
+    
+    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
         'LLL:EXT:cfc_league/locallang_db.xml:tx_cfcleague_folder',
         'cfcleague',
-    );
-    /*
-     * $PAGES_TYPES['cfcleague'] = Array(
-     * 'type' => 'sys',
-     * 'icon' => tx_rnbase_util_Extensions::extRelPath($_EXTKEY).'ext_icon_cfcleague_folder.gif',
-     * 'allowedTables' => '*'
-     * );
-     */
+        'ext-cfcleague-ext-default',
+    ];
 }
