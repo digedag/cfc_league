4eaf7254c6656f2b02d213fd8a68a237ff19aebf
René Nitzsche <rene@system25.de>
2020-05-30 12:59:20 +0200

    #62 add composer dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '7.6.0-10.4.99',
             'php' => '5.6.0-8.9.99',
             'rn_base' => '1.11.4-0.0.0',
         ],
