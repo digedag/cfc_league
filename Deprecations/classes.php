5922f28145387afaabb188d9ce1812866432ac36
René Nitzsche <rene@system25.de>
2021-10-04 18:22:20 +0200

    Change linter in pipeline

--- Deprecations/classes.php
+++ Deprecations/classes.php
@@ -1,29 +1,50 @@
 <?php
 
-class tx_cfcleague_tca_Lookup extends System25\T3sports\Utility\TcaLookup
-{
+if (class_exists('tx_cfcleague_util_Misc')) {
+    return;
 }
 
-interface tx_cfcleague_sports_ISports extends System25\T3sports\Sports\ISports
-{
-}
+\class_alias(\System25\T3sports\Utility\Misc::class, 'tx_cfcleague_util_Misc');
+\class_alias(\System25\T3sports\Utility\TcaLookup::class, 'tx_cfcleague_tca_Lookup');
 
-class tx_cfcleague_sports_Football extends System25\T3sports\Sports\Football
-{
-}
+\class_alias(\System25\T3sports\Sports\ISports::class, 'tx_cfcleague_sports_ISports');
+\class_alias(\System25\T3sports\Sports\Football::class, 'tx_cfcleague_sports_Football');
+\class_alias(\System25\T3sports\Sports\Handball::class, 'tx_cfcleague_sports_Handball');
+\class_alias(\System25\T3sports\Sports\IceHockey::class, 'tx_cfcleague_sports_IceHockey');
+\class_alias(\System25\T3sports\Sports\Volleyball::class, 'tx_cfcleague_sports_Volleyball');
+\class_alias(\System25\T3sports\Sports\MatchInfo::class, 'tx_cfcleague_sports_MatchInfo');
 
-class tx_cfcleague_sports_Handball extends System25\T3sports\Sports\Handball
-{
-}
-
-class tx_cfcleague_sports_Volleyball extends System25\T3sports\Sports\Volleyball
-{
-}
-
-class tx_cfcleague_sports_IceHockey extends System25\T3sports\Sports\IceHockey
-{
-}
-
-class tx_cfcleague_sports_MatchInfo extends System25\T3sports\Sports\MatchInfo
-{
+if (false) {
+    /** @deprecated */
+    class tx_cfcleague_util_Misc
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_tca_Lookup
+    {
+    }
+    /** @deprecated */
+    interface tx_cfcleague_sports_ISports
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_sports_Football
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_sports_Handball
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_sports_IceHockey
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_sports_Volleyball
+    {
+    }
+    /** @deprecated */
+    class tx_cfcleague_sports_MatchInfo
+    {
+    }
 }
