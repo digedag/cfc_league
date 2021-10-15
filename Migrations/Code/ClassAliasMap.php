d52fb2c9dbd89d2b2314e2b711b809082472ea17
René Nitzsche <rene@system25.de>
2021-10-15 12:20:03 +0200

    #59 Fix class aliases

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -1,15 +1,17 @@
 <?php
 
-if (class_exists('tx_cfcleague_util_Misc')) {
-    return;
-}
 
-\class_alias(\System25\T3sports\Utility\Misc::class, 'tx_cfcleague_util_Misc');
-\class_alias(\System25\T3sports\Utility\TcaLookup::class, 'tx_cfcleague_tca_Lookup');
+declare(strict_types=1);
 
-\class_alias(\System25\T3sports\Sports\ISports::class, 'tx_cfcleague_sports_ISports');
-\class_alias(\System25\T3sports\Sports\Football::class, 'tx_cfcleague_sports_Football');
-\class_alias(\System25\T3sports\Sports\Handball::class, 'tx_cfcleague_sports_Handball');
-\class_alias(\System25\T3sports\Sports\IceHockey::class, 'tx_cfcleague_sports_IceHockey');
-\class_alias(\System25\T3sports\Sports\Volleyball::class, 'tx_cfcleague_sports_Volleyball');
-\class_alias(\System25\T3sports\Sports\MatchInfo::class, 'tx_cfcleague_sports_MatchInfo');
+return [
+    'tx_cfcleague_util_Misc' => \System25\T3sports\Utility\Misc::class,
+    'tx_cfcleague_tca_Lookup' => \System25\T3sports\Utility\TcaLookup::class,
+
+    'tx_cfcleague_sports_ISports' => \System25\T3sports\Sports\ISports::class,
+    'tx_cfcleague_sports_Football' => \System25\T3sports\Sports\Football::class,
+    'tx_cfcleague_sports_Handball' => \System25\T3sports\Sports\Handball::class,
+    'tx_cfcleague_sports_IceHockey' => \System25\T3sports\Sports\IceHockey::class,
+    'tx_cfcleague_sports_Volleyball' => \System25\T3sports\Sports\Volleyball::class,
+    'tx_cfcleague_sports_MatchInfo' => \System25\T3sports\Sports\MatchInfo::class,
+];
+
