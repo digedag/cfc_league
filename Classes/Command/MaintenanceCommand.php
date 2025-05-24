77d179c1d72aebf9e66c0a79f322c93f3bfcf1b7
René Nitzsche <rene@system25.de>
2025-05-24 17:53:54 +0200

    Support for speaking url #104

---
+++ Classes/Command/MaintenanceCommand.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace System25\T3sports\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Sys25\RnBase\Database\Connection;
+use System25\T3sports\Model\Repository\MatchRepository;
+use System25\T3sports\Utility\SlugModifier;
+use Throwable;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2025 Rene Nitzsche
+ *  Contact: rene@system25.de
+ *  All rights reserved
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ ***************************************************************/
+
+/**
+ * @author Rene Nitzsche
+ */
+class MaintenanceCommand extends Command
+{
+    /** @var OutputInterface */
+    private $output;
+    private $matchRepo;
+    private $slugModifier;
+
+    public function __construct(SlugModifier $slugModifier, MatchRepository $matchRepo)
+    {
+        parent::__construct(null);
+        $this->slugModifier = $slugModifier;
+        $this->matchRepo = $matchRepo;
+    }
+
+    protected function configure()
+    {
+        $this->addArgument('task', InputOption::VALUE_REQUIRED, 'Task to execute');
+        $this->addOption('force', null, InputOption::VALUE_NONE, 'Really execute command.');
+        $this->setHelp('Maintenance operations.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+        $style = new SymfonyStyle($input, $output);
+        $style->title('T3sports maintenance');
+
+        $result = Command::SUCCESS;
+        $force = $input->getOption('force') ?? false;
+        $command = $input->getArgument('task');
+        switch ($command) {
+            case 'slug':
+                $result = $this->updateSlug($style, $force);
+                break;
+            default:
+                $style->error('Unknown command');
+
+                return Command::FAILURE;
+        }
+        $style->success('Done');
+
+        return $result;
+    }
+
+    public function updateSlug(SymfonyStyle $style, bool $force): int
+    {
+        $connection = Connection::getInstance();
+        $rows = $connection->doSelect('f.uid', [
+            'table' => 'tx_cfcleague_games',
+            'alias' => 'f',
+        ], [
+            'enablefieldsbe' => 1,
+            'debug' => 1,
+            'where' => function (QueryBuilder $qb) {
+                $qb->where(sprintf('(f.slug IS NULL OR f.slug = \'\')'));
+            },
+        ]);
+
+        $style->info(sprintf('Found %d matches without slug', count($rows)));
+
+        if (!$force) {
+            $style->comment('use --force to really execute the command');
+
+            return Command::SUCCESS;
+        }
+
+        $progress = $style->createProgressBar(count($rows));
+        $progress->setFormat('debug');
+        $progress->start();
+        foreach ($rows as $row) {
+            try {
+                $match = $this->matchRepo->findByUid($row['uid']);
+                if ($match) {
+                    $slug = $this->slugModifier->handleFixture(['record' => $match->getProperties()], $match);
+                    $match->setProperty('slug', $slug);
+                    $connection->doUpdate('tx_cfcleague_games', sprintf('uid = %d', $row['uid']), [
+                        'slug' => $slug,
+                    ]);
+                } else {
+                    $this->output->writeln(sprintf('Match %d not found', $row['uid']));
+                }
+            } catch (Throwable $e) {
+                $this->output->writeln(sprintf('Error for match (%d): %s', $row['uid'], $e->getMessage()));
+            }
+            $progress->advance();
+        }
+        $progress->finish();
+
+        return Command::SUCCESS;
+    }
+}
---
+++ Classes/Utility/SlugModifier.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace System25\T3sports\Utility;
+
+use System25\T3sports\Model\Repository\CompetitionRepository;
+use System25\T3sports\Model\Repository\SaisonRepository;
+use System25\T3sports\Model\Repository\TeamRepository;
+
+class SlugModifier
+{
+    private $competitionRepo;
+    private $saisonRepo;
+    private $teamRepo;
+
+    public function __construct(
+        CompetitionRepository $competitionRepo,
+        SaisonRepository $saisonRepo,
+        TeamRepository $teamRepo
+    ) {
+        $this->competitionRepo = $competitionRepo;
+        $this->saisonRepo = $saisonRepo;
+        $this->teamRepo = $teamRepo;
+    }
+
+    public function handleFixture(array $hookParameters, $parent)
+    {
+        $record = $hookParameters['record'];
+        $comp = $this->competitionRepo->findByUid($record['competition']);
+        $saison = $this->saisonRepo->findByUid($comp->getProperty('saison'));
+        $home = $this->teamRepo->findByUid($record['home']);
+        $guest = $this->teamRepo->findByUid($record['guest']);
+
+        $map = [
+            'comp_name' => $comp->getProperty('name'),
+            'comp_short' => $comp->getProperty('name_short') ?: $comp->getProperty('name'),
+            'comp_slug' => $comp->getProperty('slug') ?: ($comp->getProperty('name_short') ?: $comp->getProperty('name')),
+            'saison_name' => $saison->getProperty('name'),
+            'guest_name' => $guest->getProperty('name'),
+            'guest_short' => $guest->getProperty('short_name') ?: $guest->getProperty('name'),
+            'guest_tlc' => $guest->getProperty('tlc') ?: ($guest->getProperty('short_name') ?: $guest->getProperty('name')),
+            'home_name' => $home->getProperty('name'),
+            'home_short' => $home->getProperty('short_name') ?: $home->getProperty('name'),
+            'home_tlc' => $home->getProperty('tlc') ?: ($home->getProperty('short_name') ?: $home->getProperty('name')),
+        ];
+
+        $template = '[saison_name]/[comp_name]/[home_tlc]-[guest_tlc]';
+
+        $slug = preg_replace_callback('/\[([^\]]+)\]/', function ($matches) use ($map) {
+            $key = $matches[1];
+
+            return isset($map[$key]) ? $map[$key] : '';
+        }, $template);
+
+        $slug = mb_strtolower($slug, 'utf-8');
+        if (function_exists('iconv')) {
+            $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $slug);
+        } else {
+            $slug = strtr($slug, ['ä' => 'ae', 'ö' => 'oe', 'ü' => 'ue', 'ß' => 'ss']);
+        }
+        $slug = preg_replace('/[^a-z0-9-]+/', '-', $slug); // Rest normalisieren
+        $slug = preg_replace('/-+/', '-', $slug); // Mehrere - zu einem
+        $slug = trim($slug, '-/');
+
+        return $slug;
+    }
+}
